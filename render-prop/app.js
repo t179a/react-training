58242139a596c1195cab2ca7303daead49b4b183
t179a <67852630+t179a@users.noreply.github.com>
2020-10-19 15:47:13 +0900

    Add files via upload

---
+++ render-prop/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const React = require('react');
+
+class Mouse extends React.Component{
+  constructor(props){
+    super(props);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.state = {x: 0, y:0}
+  };
+
+  handleMouseMove(event){
+    this.setState({
+      x: event.clientX,
+      y: event.clientY
+    });
+  }
+
+  render(){
+    return (
+      <div style={{height: '100vh'}} onMouseMove={this.handleMouseMove}>
+
+        <p>The current mouse position is ({this.state.x}, {this.state.y})</p>
+      </div>
+    );
+  }
+}
+
+export default class App extends React.Component{
+  render(){
+    return (
+      <div>
+        <h1>Move the mouse around!</h1>
+        <Mouse />
+      </div>
+    )
+  }
+}
