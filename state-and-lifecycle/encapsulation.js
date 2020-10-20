759a55458d5fcac591144649764759715082200c
t179a <67852630+t179a@users.noreply.github.com>
2020-10-20 14:16:06 +0900

    Create encapsulation.js

---
+++ state-and-lifecycle/encapsulation.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+function Clock(props){
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+  <h2>It is {props.date.toLocaleTimeString()}</h2>
+    </div>
+  );
+}
+
+function tick(){
+  ReactDOM.render(
+    <Clock date={new Date()}/>,
+    document.getElementById('root')
+  );
+}
+
+setInterval(tick, 1000);
